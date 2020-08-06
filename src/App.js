c7cc74e1811696d052a2cbe89f0dc78a6a32013c
Nelson-E-S <nelson.e.sanabria1@gmail.com>
2020-08-06 13:51:49 -0400

    first pass card styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CardBox from './component/CardBox';
+import imageOne from './images/1208.jpg'
 
 function App() {
   return (
     <div className="App">
-      
+      <CardBox 
+        cardType="short"
+        cardFlavor="news"
+        isQuote={true}
+        author="Ankita Rao"
+        imageAdd={true}
+        imageSrc={imageOne}
+        widthMod = {2}
+        headLine="Testing"
+        subLine="This a test design" 
+        descAdd={false}
+        descLine="This is a test description line" />
     </div>
   );
 }
--- src/component/CardBox.js
+++ src/component/CardBox.js
@@ -4,23 +4,31 @@ export default class CardBox extends Component{
     constructor(props){
         super(props);
         this.state = {
-            width: '220px'
+            width: '220'
         }
         //expected props:
-        //cardType = big / small
+        //cardType = tall / short
+        //cardFlavor = news / opinion / sport / culture / lifestyle
         //widthMod = 1 / 2 / 3
-        //imageSRC = {imageSRC} from parent
+        //imageAdd = true / false
+        //imageSrc = {imageSrc} from parent
         //headLine = "headline text"
         //subLine  = "subline text"
         //descAdd  = true / false
         //descLine = "description text"
     }
     render(){
+        let artClass = `${this.props.cardType} ${this.props.cardFlavor}`
+        let artStyle = {
+            width: `${this.props.widthMod * this.state.width}px`
+        }
         return(
-            <article>
-                {this.props.cardType==="big"?<div><img src="#" alt="#" /><br /></div>:''}
-                <p><h6>{this.props.headline}</h6> / {this.props.subline}</p>
-                {this.props.descAdd?<p>{this.props.descLine}</p>:''}
+            <article className={artClass} style={artStyle}>
+                {this.props.imageAdd?<img src={this.props.imageSrc} alt="card" />:''}
+                <div>
+        <p className="title"><span className="headLine">{this.props.headLine}</span> {this.props.headLine?'/':''} {this.props.isQuote?<span className="quote">"</span>:''}{this.props.subLine}{this.props.isQuote?<div><em className="author">{this.props.author}</em></div>:''}</p>
+                    {this.props.descAdd?<p className="description">{this.props.descLine}</p>:''}
+                </div>
             </article>
         );
     }
